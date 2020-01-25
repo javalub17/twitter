487f7744bf2c31171db9e68310ea76ab275d531d
lkre <lukasz.res@gmail.com>
2020-01-25 09:10:01 +0100

    usunięcie komentarzy

--- src/main/java/com/sda/twitter/persistance/HibernateUtil.java
+++ src/main/java/com/sda/twitter/persistance/HibernateUtil.java
@@ -1,45 +1,5 @@
 package com.sda.twitter.persistance;
 
-//import org.hibernate.SessionFactory;
-//import org.hibernate.boot.Metadata;
-//import org.hibernate.boot.MetadataSources;
-//import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-//import org.hibernate.service.ServiceRegistry;
-//import org.hibernate.cfg.Configuration;
-//
-//public class HibernateUtil {
-//    private static final SessionFactory SESSION_FACTORY = buildSessionFactory();
-//
-//    private static SessionFactory buildSessionFactory() {
-////        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-////                .configure("hibernate.cfg.xml").build();
-////        Metadata metadata = new MetadataSources(serviceRegistry)
-////                .getMetadataBuilder().build();
-////        return metadata.getSessionFactoryBuilder().build();
-//        try {
-////            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()//
-////                    .configure("hibernate.cfg.xml").build();
-////            Metadata metadata = new MetadataSources(serviceRegistry).getMetadataBuilder().build();
-////            return metadata.getSessionFactoryBuilder().build();
-//            return new Configuration()
-//                    .configure()
-//                    .buildSessionFactory();
-//        }catch (Throwable ex) {
-//            System.err.println("Initial SessionFactory creation failed." + ex);
-//            throw new ExceptionInInitializerError(ex);
-//        }
-//
-//    }
-//
-//    public static SessionFactory getSessionFactory() {
-//        return SESSION_FACTORY;
-//    }
-//
-//    public static void shutDown() {
-//        getSessionFactory().close();
-//    }
-//
-//}
 
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -54,11 +14,6 @@ public class HibernateUtil {
 
     private static SessionFactory buildSessionFactory() {
         try {
-//            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()//
-//                    .configure("hibernate.cfg.xml").build();
-//            Metadata metadata = new MetadataSources(serviceRegistry).getMetadataBuilder().build();
-//            return metadata.getSessionFactoryBuilder().build();
-            //                    .build();
             return new Configuration()
                     .configure()
                     .buildSessionFactory();
